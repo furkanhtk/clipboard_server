ec631bff6cdd6a183cf5048a27d3ed7b266e853b
furkanhatikAGADE <furkan.hatik@agade.eu>
2023-06-06 00:45:01 +0200

    Test2

--- server.py
+++ server.py
@@ -1,42 +1,6 @@
-from flask import Flask, render_template, request, jsonify
-import base64
-from PIL import Image
-from io import BytesIO
-import socket
-
+from flask import Flask, render_template
 app = Flask(__name__)
-clipboard_data = []
-port_number=5000
 
 @app.route('/')
-def home():
-    return render_template('index.html', clipboard_items=clipboard_data)
-
-@app.route('/clipboard', methods=['POST'])
-def clipboard():
-    global clipboard_data
-
-    # Get the clipboard text and the screenshot data from the request
-    data = request.get_json()
-    clipboard_text = data.get('clipboard_text')
-    screenshot_base64 = data.get('screenshot_base64')
-
-    # Add the clipboard item to the list
-    clipboard_data.append(clipboard_text)
-
-    # Convert the screenshot from base64 to PIL Image
-    screenshot_bytes = base64.b64decode(screenshot_base64)
-    screenshot_image = Image.open(BytesIO(screenshot_bytes))
-
-    # Save the screenshot image to a file
-    screenshot_path = "static/screenshot.png"
-    screenshot_image.save(screenshot_path)
-
-    return "Clipboard data and screenshot updated successfully!"
-
-@app.route('/check_connection')
-def check_connection():
-    return jsonify({'status': 'online'})
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=port_number)
+def hello_world():
+    return render_template('hello.html')
