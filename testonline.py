03a8ae666c6f5e27c18d89e4f7f494846a2562e0
furkanhatikAGADE <furkan.hatik@agade.eu>
2023-06-06 00:48:33 +0200

    test01

--- testonline.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-response = requests.get('http://10.100.0.62:5000/check_connection')
-if response.status_code == 200:
-    data = response.json()
-    if data['status'] == 'online':
-        print('Server is online')
-else:
-    print('Server is not reachable')
