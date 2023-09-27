d9df94f35fda87c57eb6f1b0acbf3620f006c56c
furkanhatikAGADE <furkan.hatik@agede.eu>
2023-09-27 14:19:35 +0200

    Test3

--- app.py
+++ app.py
@@ -1,23 +1,32 @@
-from flask import Flask, request, jsonify
+import os
+
+from flask import (Flask, redirect, render_template, request,
+                   send_from_directory, url_for)
 
 app = Flask(__name__)
 
-# Store the clipboard data in a variable (you can use a database for this purpose)
-clipboard_data = ""
 
-@app.route('/copy', methods=['POST'])
-def copy_text():
-    global clipboard_data
-    data = request.json
-    clipboard_data = data.get('clipboard_data')
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
 
-    return jsonify({'message': 'Clipboard data received successfully'}), 200
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/paste', methods=['GET'])
-def paste_text():
-    global clipboard_data
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
 
-    return jsonify({'clipboard_data': clipboard_data}), 200
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+   app.run()
