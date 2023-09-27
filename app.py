88820b923343885dd81765c711184347b69a04b9
furkanhatikAGADE <furkan.hatik@agede.eu>
2023-09-27 17:28:30 +0200

    logo removed

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ def index():
    print('Request for index page received')
    return render_template('index.html')
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+# @app.route('/favicon.ico')
+# def favicon():
+#     return send_from_directory(os.path.join(app.root_path, 'static'),
+#                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/hello', methods=['POST'])
 def hello():
