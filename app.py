da4f899b56315c86b88b775611b8fc01dfcb29d3
furkanhatikAGADE <furkan.hatik@agede.eu>
2023-09-27 17:33:51 +0200

    copy paste added

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
 
-from flask import (Flask, redirect, render_template, request,
+from flask import (Flask, redirect, render_template, request, jsonify,
                    send_from_directory, url_for)
 
 app = Flask(__name__)
+
+clipboard_data = ""
 
 
 @app.route('/')
@@ -15,6 +17,22 @@ def index():
 # def favicon():
 #     return send_from_directory(os.path.join(app.root_path, 'static'),
 #                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/copy', methods=['POST'])
+def copy_text():
+    global clipboard_data
+    data = request.json
+    clipboard_data = data.get('clipboard_data')
+
+    return jsonify({'message': 'Clipboard data received successfully'}), 200
+
+
+@app.route('/paste', methods=['GET'])
+def paste_text():
+    global clipboard_data
+
+    return jsonify({'clipboard_data': clipboard_data}), 200
+
 
 @app.route('/hello', methods=['POST'])
 def hello():
